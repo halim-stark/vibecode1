114d7374a8bd68c7f4bff804269853a183cd4994
Halim <halimtsaqif05@gmail.com>
2026-09-20 13:45:19 +0800

    Test DSA stack queue

--- test.py
+++ test.py
@@ -1,16 +1,42 @@
 numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45]
-target = 35
+target = 5
 
 right = len(numbers) - 1
-left = 0 
-mid_float = (right + left) / 2
-mid = int(mid_float)
-print(numbers[mid]) 
-print(mid)
+left = 0
 
-right = len(numbers) - 1
-left = mid + 1      
-mid_float = (right + left) / 2
-mid = int(mid_float)
-print(numbers[mid]) 
+while left <= right:
+    mid = 0
 
+    midt = (left + right) // 2
+
+    if numbers[mid] == target:
+        final = numbers[mid]
+        break
+
+    elif numbers[mid] < target:
+        left = mid + 1
+        mid = (left + right) // 2
+        final = numbers[mid]
+
+    elif numbers[mid] > target:        
+        left = 0
+        right = mid - 1
+        mid = (left + right) // 2
+        final = numbers[mid]
+print(final)
+
+
+
+stack = []
+stack.append("A")
+stack.append("B")
+stack.append("C")
+
+hasil = stack.pop()
+
+print(hasil)
+print(stack)
+
+
+
+                     
